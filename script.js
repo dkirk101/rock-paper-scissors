311cd60e66975e97d24aed8f50604753dc4f269c
dkirk101 <150102252+dkirk101@users.noreply.github.com>
2024-02-17 23:16:23 -0500

    Add instructions from TOP and pseudocode

---
+++ script.js
@@ -0,0 +1,11 @@
+// Game will be played against the computer - start with get getComputerChoice function - rock, paper, scissors
+// Create a function that plays a single round, includes the player's and computer's choice
+    // function should be case insensitive and declare the winner
+// Create a game function to play 5 rounds, keeping track of winner and loser
+
+
+// Declare a variable via user prompt for user's selection of rock, paper, or scissors
+// Validate players selection
+// Create function for the computer to select either rock, paper, or scissors randomly and is case insensitive
+// Create a function to play a single round of game using the user and computer computer choices
+// Create afunction to execute the round 5 times, outputting and tracking the winner and loser of each round
